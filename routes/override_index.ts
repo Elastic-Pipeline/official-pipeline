1c7ed6a7bf660353aeb3d41776d2084aa38af73a
IronCPlusPlus <bsmithcompsci@gmail.com>
2021-05-30 23:48:35 -0700

    Testing Table Factory

--- routes/override_index.ts
+++ routes/override_index.ts
@@ -1,3 +1,4 @@
+import { TableFactory } from "../../../API/RenderBits/TableFactory";
 import { Route, ROUTE_FIRST } from "../../../API/Routing/Routing";
 
 export class OverrideIndexPipelineRoute extends Route
@@ -8,7 +9,31 @@ export class OverrideIndexPipelineRoute extends Route
 
         this.Get("", async (req, res, next) => 
         {
-            res.render('views/index');
+            const projectsTable = new TableFactory();
+            projectsTable.SetHeader(['', 'Name', 'Last Successful', 'Last Failed', '']);
+            projectsTable.AddRow(
+                '<span class="badge bg-success rounded-pill">Successful</span>', 
+                'Good Job', 
+                '11h 30m <a href="#">#1</a>', 
+                'Never', 
+                '<a class="btn btn-sm btn-primary" href="#"><i class="fa fa-cogs" aria-hidden="true"></i></a>'
+            );
+            projectsTable.AddRow(
+                '<span class="badge bg-warning rounded-pill">Unstable</span>', 
+                'Unstable Job', 
+                '5m <a href="#">#2</a>', 
+                '5h 00m <a href="#">#1</a>', 
+                '<a class="btn btn-sm btn-primary" href="#"><i class="fa fa-cogs" aria-hidden="true"></i></a>'
+            );
+            projectsTable.AddRow(
+                '<span class="badge bg-danger rounded-pill">Failed</span>', 
+                'Bad Job', 
+                'Never', 
+                '2h 30m <a href="#">#4</a>', 
+                '<a class="btn btn-sm btn-primary" href="#"><i class="fa fa-cogs" aria-hidden="true"></i></a>'
+            );
+            projectsTable.SetTableClass("table");
+            res.render('views/index', { projectsTable: projectsTable.Display() });
         }, "home", ROUTE_FIRST);
     }
 }
